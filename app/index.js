cb0ec86416a17925f4ade6561029d4921e813107
austin1srz <austin1srz@gmail.com>
2024-06-12 15:49:18 -0600

    SDI Proj 1

--- app/index.js
+++ app/index.js
@@ -1,15 +1,50 @@
-document.addEventListener("DOMContentLoaded",() => {
-    var toDos = [
-    {done:false, text:"display todos to user"},
-    {done: false, text:"allow user to add a todo"},
-    {done: false, text:"allow user to mark as completed"},
-    {done: false, text: "allow user to remove a to-do task"},
-    {done: false, text: "style"}
-    ];
-    var toDoListElement = document.querySelector("#todo-list")
-    toDos.map((todo) =>{
-        var li = document.createElement("li")
-        li.innerHTML = todo.text
-        toDoListElement.appendChild(li)
-    }) 
-}) 
+document.addEventListener("DOMContentLoaded", () => {
+  var toDoListElement = document.getElementById("todo-list");
+  var addToDoButton = document.getElementById("add-todo-btn");
+
+  const btn = document.getElementById("add-todo-btn");
+  const todoInput = document.getElementById("todo-input");
+
+  var todoText = "";
+
+  todoInput.addEventListener("input", (e) => {
+    todoText = e.currentTarget.value;
+  });
+
+  btn.addEventListener("click", () => {
+    var liElement = document.createElement("li");
+    var removeButton = document.createElement("button");
+    var spanEl = document.createElement("span");
+
+    removeButton.classList.add("delete");
+    removeButton.classList.add("is-small");
+
+    spanEl.innerText = todoText;
+    liElement.appendChild(spanEl);
+    liElement.appendChild(removeButton);
+
+    removeButton.addEventListener("click", deleteItem);
+    liElement.addEventListener("click", strikethrough);
+
+    toDoListElement.appendChild(liElement);
+
+    todoInput.value = "";
+
+  });
+
+  var deleteItem = (e) => {
+    const liElement = e.target.parentElement;
+    toDoListElement.removeChild(liElement);
+
+  }
+
+  function strikethrough(e) {
+    const liElement = e.currentTarget;
+
+    if (liElement.classList.contains("done")) {
+      liElement.classList.remove("done");
+    } else {
+      liElement.classList.add("done");
+    }
+  }
+});
